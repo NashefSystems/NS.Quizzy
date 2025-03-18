43bae83773a98e117ed94e7895f7dd2f441587a5
Nashef <nashef90@gmail.com>
2025-03-18 22:44:13 +0200

    Revert "EOD"
    This reverts commit 9d8ec33f8921b70a128ff697e8d105e6df3f9b9c.

--- ns.quizzy.client/src/app/components/auth-area/login-with-id-number/login-with-id-number.component.ts
+++ ns.quizzy.client/src/app/components/auth-area/login-with-id-number/login-with-id-number.component.ts
@@ -25,7 +25,7 @@ export class LoginWithIdNumberComponent implements OnInit {
   private readonly _storageService = inject(StorageService);
 
   loginForm: FormGroup = this._fb.group({
-    idNumber: ['', [Validators.required]],
+    idNumber: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
     rememberMe: [true],
   });
 
