a67eb5e88f80444ecf48bf81e243f739d65ed504
JUMPBOX\Nashef <Nashef90@gmail.com>
2025-09-14 08:07:52 +0300

    Change DemoUser permission

---
+++ NS.Quizzy.Server.DAL/Migrations/20250914050729_Change DemoUser permission.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NS.Quizzy.Server.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDemoUserpermission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("f8178f51-6d73-45e1-a4e7-1b01baf9884d"),
+                column: "Role",
+                value: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("f8178f51-6d73-45e1-a4e7-1b01baf9884d"),
+                column: "Role",
+                value: 2);
+        }
+    }
+}
