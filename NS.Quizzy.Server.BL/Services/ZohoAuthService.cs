61b9ab00fe1b6fc0c02494018d65917c6c1b1fce
Nashef <nashef90@gmail.com>
2025-12-03 20:14:25 +0200

    eee

--- NS.Quizzy.Server.BL/Services/ZohoAuthService.cs
+++ NS.Quizzy.Server.BL/Services/ZohoAuthService.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using NS.Quizzy.Server.BL.Interfaces;
+using NS.Shared.CacheProvider.Interfaces;
 using System.Net.Http.Json;
 using System.Text.Json;
 
@@ -8,15 +10,18 @@ namespace NS.Quizzy.Server.BL.Services
     internal class ZohoAuthService : IZohoAuthService
     {
         private readonly HttpClient _httpClient;
+        private readonly INSCacheProvider _cacheProvider;
         private readonly string _clientId;
         private readonly string _clientSecret;
         private readonly string _refreshToken;
         private readonly string _redirectUri;
         private readonly List<string> _scopes;
+        private const string CACHE_KEY = "ZohoAuthService.AccessTokenResponse";
 
-        public ZohoAuthService(IConfiguration configuration, HttpClient httpClient)
+        public ZohoAuthService(IConfiguration configuration, HttpClient httpClient, INSCacheProvider cacheProvider)
         {
             _httpClient = httpClient;
+            _cacheProvider = cacheProvider;
 
             _clientId = configuration.GetValue<string>("Zoho:ClientId", string.Empty);
             _clientSecret = configuration.GetValue<string>("Zoho:ClientSecret", string.Empty);
@@ -57,6 +62,12 @@ namespace NS.Quizzy.Server.BL.Services
         // Get access token using refresh token (use in your service)
         public async Task<string> GetAccessToken()
         {
+            var accessTokenResponse = await _cacheProvider.GetAsync<AccessTokenResponse>(CACHE_KEY);
+            if (accessTokenResponse != null)
+            {
+                return accessTokenResponse.AccessToken;
+            }
+
             var content = new FormUrlEncodedContent(new[]
             {
                 new KeyValuePair<string, string>("grant_type", "refresh_token"),
@@ -67,9 +78,37 @@ namespace NS.Quizzy.Server.BL.Services
 
             var response = await _httpClient.PostAsync(
                 "https://accounts.zoho.com/oauth/v2/token", content);
-            var result = await response.Content.ReadFromJsonAsync<JsonElement>();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"({response.StatusCode}) {responseContent}");
+            }
 
-            return result.GetProperty("access_token").GetString();
+            accessTokenResponse = JsonConvert.DeserializeObject<AccessTokenResponse>(responseContent);
+            if (accessTokenResponse == null)
+            {
+                throw new Exception($"accessTokenResponse is null");
+            }
+            await _cacheProvider.SetOrUpdateAsync(CACHE_KEY, accessTokenResponse, TimeSpan.FromSeconds(accessTokenResponse.ExpiresIn - 10));
+            return accessTokenResponse.AccessToken;
+        }
+
+        class AccessTokenResponse
+        {
+            [JsonProperty("access_token")]
+            public string AccessToken { get; set; }
+
+            [JsonProperty("scope")]
+            public string Scope { get; set; }
+
+            [JsonProperty("api_domain")]
+            public string ApiDomain { get; set; }
+
+            [JsonProperty("token_type")]
+            public string TokenType { get; set; }
+
+            [JsonProperty("expires_in")]
+            public int ExpiresIn { get; set; }
         }
     }
 }
